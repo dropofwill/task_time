5987733dd4add62466d9f2c1f22576bba119d945
Will <whp3652@rit.edu>
2015-03-01 22:26:32 -0500

    Add trivial test

---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'coveralls'
+require 'task_time'
+
+Coveralls.wear!
---
+++ spec/task_time_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "TaskTime" do
+  it 'has a version number' do
+    expect(TaskTime::VERSION).not_to be nil
+  end
+end
