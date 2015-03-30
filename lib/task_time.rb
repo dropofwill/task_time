348de9c389e830b603694c19f66cd368bea6ddc4
Will <whp3652@rit.edu>
2015-03-29 21:50:01 -0400

    Scaffold out basic API

--- lib/task_time.rb
+++ lib/task_time.rb
@@ -1,5 +1,37 @@
-require "task_time/version"
+require_relative "task_time/version"
+require "thor"
 
 module TaskTime
-  # Your code goes here...
+
+  class CLI < Thor
+
+    desc "start", "Start a timer on the specified task"
+    def start(filter)
+    end
+
+    desc "stop", "Stop the timer on the specified task"
+    def stop(filter)
+    end
+
+    desc "toggle", ""
+    def toggle(filter)
+    end
+
+    desc "edit", ""
+    def edit(filter)
+    end
+
+    desc "modify", ""
+    def modify(filter)
+    end
+
+    desc "report", ""
+    def report(filter)
+    end
+
+    desc "export", ""
+    def export(filter, format)
+    end
+
+  end
 end
