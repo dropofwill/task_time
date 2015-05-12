e150b40b2e5b90c56bd5a75c220e623c0cd6c1b0
Will <whp3652@rit.edu>
2015-05-12 03:00:21 -0400

    Write down the udas

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
+group :development do
+  gem 'rtasklib', path: "#{Dir.home}/Code/rtasklib/"
+end
+
 # Specify your gem's dependencies in task_time.gemspec
 gemspec
--- lib/task_time.rb
+++ lib/task_time.rb
@@ -8,12 +8,16 @@ module TaskTime
     task_data: "#{Dir.home}/.task"
   }
 
+  UDAS = [ "timestamps", "time-spent", "time-est",
+           "time-prog", "progress", "client" ]
+
   class TT
-    attr_accessor :config
+    attr_accessor :config, :tw
 
     def initialize config
       @config = Helpers.read_config(config)
-      @tw = Rtasklib::TW.new
+      @tw = Rtasklib::TW.new(config["data_location"])
+      puts tw.all
     end
   end
 end
