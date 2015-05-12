a154f21bb0c2770aa3ba33284dbf7e947c708211
Will <whp3652@rit.edu>
2015-05-12 00:13:12 -0400

    Add empty pre/post hooks, add default config sample

--- lib/task_time.rb
+++ lib/task_time.rb
@@ -2,50 +2,7 @@ require_relative "task_time/version"
 require "gli"
 
 module TaskTime
-  include GLI:App
 
-  program_desc "An addon to TaskWarrior that allows time tracking and reporting"
-
-  desc "start", "Start a timer on the specified task"
-  command :start do |c|
-    c.action do |glob, opts, args|
-    end
-  end
-
-  desc "stop", "Stop the timer on the specified task"
-  command :stop do |c|
-    c.action do |glob, opts, args|
-    end
-  end
-
-  desc "toggle", ""
-  command :toggle do |c|
-    c.action do |glob, opts, args|
-    end
-  end
-
-  desc "edit", ""
-  command :edit do |c|
-    c.action do |glob, opts, args|
-    end
-  end
-
-  desc "modify", ""
-  command :modify do |c|
-    c.action do |glob, opts, args|
-    end
-  end
-
-  desc "report", ""
-  command :report do |c|
-    c.action do |glob, opts, args|
-    end
-  end
-
-  desc "export", ""
-  command :export do |c|
-    c.action do |glob, opts, args|
-    end
-  end
+  DEFAULTS = {}
 
 end
